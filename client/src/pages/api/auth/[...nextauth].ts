1a4f19ab47d5d827bf40251ecac652f3ea018853
Volodymyr Halchuk <volodymyr.halchuk.dev@gmail.com>
2023-02-08 21:58:35 +0400

    Add auth

---
+++ client/src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,16 @@
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import prisma from "../../../lib/prismadb"
+import { env } from "@/env/server.mjs";
+
+export default NextAuth({
+    adapter: PrismaAdapter(prisma),
+    providers: [
+        GoogleProvider({
+            clientId: env.GOOGLE_CLIENT_ID,
+            clientSecret: env.GOOGLE_CLIENT_SECRET,
+        }),
+    ],
+    secret: env.NEXTAUTH_SECRET,
+})
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,7 +1,19 @@
+import {signIn, signOut, useSession} from 'next-auth/react'
+
 export default function Home() {
-  return (
-    <>
-      Home
-    </>
-  )
+    const { data } = useSession();
+
+    return (
+        <div>
+            {data?.user ? (
+                <button onClick={() => signOut()}>
+                    Sign out
+                </button>
+            ) : (
+                <button onClick={() => signIn('google')}>
+                    Sign in
+                </button>
+            )}
+        </div>
+    );
 }
