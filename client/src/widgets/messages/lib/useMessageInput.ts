ff5cc6705c74a2a25c21f1868d19343356102a6d
Volodymyr Halchuk <volodymyr.halchuk.dev@gmail.com>
2023-02-25 09:19:50 +0400

    Extract MessageInput logic into useMessageInput

---
+++ client/src/widgets/messages/lib/useMessageInput.ts
@@ -0,0 +1,54 @@
+import { ObjectID } from 'bson';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
+import { FormEventHandler, useState } from 'react';
+import toast from 'react-hot-toast';
+import { useSendMessageAndUpdateCache } from '@/entities/message';
+import { SendMessageVariables } from '@/shared/types/messageTypes';
+
+export const useMessageInput = () => {
+  const conversationId = useRouter().query.conversationId as string;
+  const { data: session } = useSession();
+  const user = session!.user;
+
+  const [messageBody, setMessageBody] = useState('');
+
+  const sendMessageAndUpdateCache = useSendMessageAndUpdateCache();
+
+  const handleSend: FormEventHandler = async (event) => {
+    event.preventDefault();
+
+    try {
+      const newMessage: SendMessageVariables = {
+        id: new ObjectID().toString(),
+        senderId: user.id,
+        conversationId,
+        body: messageBody,
+      };
+      const sender = {
+        id: user.id,
+        username: user.username!,
+      };
+
+      const { data, errors } = await sendMessageAndUpdateCache({
+        conversationId,
+        newMessage,
+        sender,
+      });
+
+      if (!data?.sendMessage || errors) {
+        toast.error('Error sending message');
+      }
+    } catch (error: any) {
+      toast.error(error?.message);
+    } finally {
+      setMessageBody('');
+    }
+  };
+
+  return {
+    messageBody,
+    setMessageBody,
+    handleSend,
+  };
+};
--- client/src/widgets/messages/ui/MessageInput.tsx
+++ client/src/widgets/messages/ui/MessageInput.tsx
@@ -1,56 +1,13 @@
 import { Box, Input } from '@chakra-ui/react';
-import { ObjectID } from 'bson';
-import { useSession } from 'next-auth/react';
-import { useRouter } from 'next/router';
-import { type FC, useState } from 'react';
-import toast from 'react-hot-toast';
-import { useSendMessageAndUpdateCache } from '@/entities/message';
-import { SendMessageVariables } from '@/shared/types/messageTypes';
+import { type FC } from 'react';
+import { useMessageInput } from '../lib/useMessageInput';
 
 export const MessageInput: FC = () => {
-  const conversationId = useRouter().query.conversationId as string;
-  const { data: session } = useSession();
-  const user = session!.user;
-
-  const [messageBody, setMessageBody] = useState('');
-
-  const sendMessageAndUpdateCache = useSendMessageAndUpdateCache();
-
-  const onSendMessage = async (event: React.FormEvent) => {
-    event.preventDefault();
-
-    try {
-      const newMessage: SendMessageVariables = {
-        id: new ObjectID().toString(),
-        senderId: user.id,
-        conversationId,
-        body: messageBody,
-      };
-      const sender = {
-        id: user.id,
-        username: user.username!,
-      };
-
-      const { data, errors } = await sendMessageAndUpdateCache({
-        conversationId,
-        newMessage,
-        sender,
-      });
-
-      if (!data?.sendMessage || errors) {
-        toast.error('Error sending message');
-      }
-    } catch (error: any) {
-      console.log('onSendMessage error', error);
-      toast.error(error?.message);
-    } finally {
-      setMessageBody('');
-    }
-  };
+  const { messageBody, setMessageBody, handleSend } = useMessageInput();
 
   return (
     <Box px={4} py={6} width="100%">
-      <form onSubmit={onSendMessage}>
+      <form onSubmit={handleSend}>
         <Input
           value={messageBody}
           onChange={(event) => setMessageBody(event.target.value)}
